78a28eca8fdc5f9921998ee121e3c16972460d4b
MudasirIrshad <mudasirirshad47@gmail.com>
2025-02-18 00:01:10 +0500

    added prisma

--- my-turborepo/apps/user-app/src/pages/index.tsx
+++ my-turborepo/apps/user-app/src/pages/index.tsx
@@ -1,7 +1,10 @@
+import { PrismaClient } from "@repo/db/client";
+
+const client = new PrismaClient();
 export default function Home() {
   return (
     <>
-      <div></div>
+      <div className="text-5xl">hello world</div>
     </>
   );
 }
